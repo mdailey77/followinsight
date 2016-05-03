ca03093445af2433fefb2e23e749670474b8367a
Matthew Dailey <mdailey77@users.noreply.github.com>
2016-05-03 11:35:24 +0000

    removed unnecessary javascript files, added AngularJS code to app.js

--- js/app.js
+++ js/app.js
@@ -1,56 +1,94 @@
-var AngularTwitterApp = angular.module('twitterApp', ['ngResource']);
-
-AngularTwitterApp.factory('Userservice', ['$resource', function($resource){
-	return $resource("php/connect.php?id=:id");	
-}]);
-/*
-AngularTwitterApp.config(function($routeProvider) {
-    $routeProvider
-    // route for the home page
-    .when('/', {
-        templateUrl : 'pages/home.html',
-        controller  : 'mainController'
-    })
-    // route for the twitterid page
-    .when('/twitterId', {
-        templateUrl : 'pages/twitterid.html',
-        controller  : 'twitterIdController'
-    }).
-    otherwise({
-        redirectTo: '/'
-    });   
-});
-*/
-AngularTwitterApp.controller('mainController', function($scope, Userservice) {
-    $scope.loading = false;
-    $scope.counter = 0;   
-    $scope.error = false;
-    $scope.users = [];
-    $scope.getResult = function(id){   
-        $scope.loading = true;
-        Userservice.get({
-            id: id
-        }, function(user) {
-            console.log(user);  
-            if(user.error) {
-                $scope.error = true;
-                $scope.message = "Validation Error please fill the user_id or screen_name field";
-            }else{
-                if(!user.errors){
-                    $scope.counter += 1;
-                    user.position = $scope.counter;
-                    $scope.users.push(user);
-                    $scope.error = false;
-                }else{            
-                    $scope.error = true;
-                    $scope.message = user.errors[0]['message']+" - "+user.errors[0]['code'] ;
-                }               
-            }                      
-        }); 
-        $scope.loading = false;
-    };
-    
-    $scope.removeUser = function(index){
-        $scope.usuarios.splice(index, 1);
-    }; 
+angular.module('twitterApp', ['ngResource'])			
+.factory('TwitterUser', ['$resource', function($resource){
+	return $resource("php/twitterconnect.php?id=:id");	
+}])
+.factory('KloutUser', ['$resource', function($resource){
+	return $resource("php/kloutconnect.php?id=:id");
+}])
+.filter('datetime', function($filter)
+{
+	return function(input)
+	{
+		if(input == null){ return ""; }
+		var _date = $filter('date')(new Date(input),
+			'MMMM d, y - hh:mm:ss');
+		return _date;
+	};
+})
+.filter('verifiedFilter', function () {
+	return function (input) {				  
+	   if (input == true) {
+			input = 'yes';
+		} else if (input == false){
+			input = 'no';
+		}				   
+	   return input;
+	}
+})
+.filter('locationFilter', function () {
+	return function (input) {
+		var userlocation = "location not set";
+		if(input != ""){				
+			userlocation = "located in " + input;
+		}									   
+	   return userlocation;
+	}
+})
+.filter('profileimageFilter', function () {
+	return function (input) {
+	   if (input == null) {return ""; }				   
+	   var imageformat = input.substr(input.lastIndexOf('.') + 1);
+	   var trimmedinput = input.substring(0, input.indexOf('_normal'));
+	   if (imageformat == 'png') {
+			var original_profileimage = trimmedinput + '.png';
+		} else if (imageformat == 'jpg'){
+			var original_profileimage = trimmedinput + '.jpg';
+		} else if (imageformat == 'jpeg'){
+			var original_profileimage = trimmedinput + '.jpeg';
+		}
+	   return original_profileimage;
+	}
+})
+.controller('mainController', function($scope, TwitterUser, KloutUser) {
+	$scope.buttontext = "Get Insight";
+	$scope.kloutscore = null;
+	$scope.error = false;
+	$scope.users = [];
+	$scope.getResult = function(id){
+		$scope.users = [];
+		$scope.buttontext = "Loading";		
+		TwitterUser.get({
+			id: id
+		}, function(user) {						
+			if(user.error) {
+				$scope.error = true;
+				$scope.message = "Please enter a screenname.";
+			}else{
+				if(!user.errors){
+					console.log(user);
+					$scope.users.push(user);
+					$scope.error = false;
+				}else{            
+					$scope.error = true;
+					$scope.message = user.errors[0]['message']+" - "+user.errors[0]['code'] ;
+				}               
+			}                      
+		}).$promise.then(function(user){
+			KloutUser.get({
+				id: user.id
+			}, function(userkloutscore) {
+				if(!userkloutscore) {							
+					console.log('An error occurred. No Klout score returned.');
+				}else{
+					$scope.kloutscore = userkloutscore.score;
+					var score_stringified = JSON.stringify(userkloutscore);
+					console.log('The Klout API response: ' + score_stringified);
+				}
+				$scope.buttontext = "Get Insight";
+			});
+		});					
+	};				
+	$scope.removeUser = function(index){
+		$scope.users.splice(index, 1);
+	}; 
 });
